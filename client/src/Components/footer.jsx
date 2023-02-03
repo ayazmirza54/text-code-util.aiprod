6ed982c4c341a3a9595c423f6a134efe74e45cb3
Ayaz Mirza <37616943+ayazmirza54@users.noreply.github.com>
2023-02-03 15:25:49 +0530

    Done full and final

---
+++ client/src/Components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+    <p>Made with passion by <a href="#">🚀Ayaz Mirza</a></p>
+  </footer>
+  
+  )
+}
+
+export default Footer
