eb2767fa61c7fcca29ef75368f0fb6c391dc7985
Ayaz Mirza <37616943+ayazmirza54@users.noreply.github.com>
2023-02-16 18:32:51 +0530

    Done

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer>
-    <p>Made with passion by <a href="#">🚀Ayaz Mirza</a></p>
+    <p>Made with passion by <a href="#">🚀Ayaz Mirza</a> Pro Tip : Use dark mode on device for best experience 🪩</p>
   </footer>
   
   )
