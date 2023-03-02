4874f867c7bd01f78561d4cea5ae02663e19781c
Ayaz Mirza <ayazmirza54@gmail.com>
2023-03-02 14:44:33 +0530

    Donebb

--- server/script.js
+++ server/script.js
@@ -107,11 +107,6 @@ async function gettldr() {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{role: "user", content: generatetldr(tldr)}],
-      temperature: 0.7,
-      max_tokens: 300,
-      top_p: 1.0,
-      frequency_penalty: 0.2,
-      presence_penalty: 0.0,
     });
     console.log(generatetldr(tldr));
     console.log(completion.data.choices[0].message.content);
